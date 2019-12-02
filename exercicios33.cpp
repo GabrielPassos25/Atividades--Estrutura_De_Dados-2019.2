86b0b520407676bc9fc1a76cc2e7a24bf7fd54b0
PET Computação <petcomp@ufc.br>
2019-12-02 09:21:04 -0300

    Correção ex.33

--- exercicios33.cpp
+++ exercicios33.cpp
@@ -57,34 +57,39 @@ struct ArvoreBinaria{
         if(raiz==nullptr){
             return;
         }
-        int a = fator_balanceamento(raiz);
         em_ordem_nivel_balanceamento(raiz->fesq);
+        int a = fator_balanceamento(raiz);
         cout << "Fator de balanceamento: " << a << " Valor do elemento: " << raiz->elem << "\n";
         em_ordem_nivel_balanceamento(raiz->fdir);
     }
     int fator_balanceamento(Noh* raiz){
         if(raiz == nullptr){
-            return -1;
+            return 0;
         }
-        int esq = fator_balanceamento(raiz->fesq);
-        int dir = fator_balanceamento(raiz->fdir);
-        cout << esq << "e\n";
-        cout << dir <<"d\n";
-        return dir-esq;
+        return altura(raiz->fdir) - altura(raiz->fesq);
     }
+    int altura (Noh* r) {
+    if (r == NULL) 
+        return -1; // altura da árvore vazia
+    else {
+        int he = altura (r->fesq);
+        int hd = altura (r->fdir);
+        if (he < hd) return hd + 1;
+        else return he + 1;
+    }
+}
+
 };
 int main(){
     ArvoreBinaria<int> b;
-    b.inserir(30);
     b.inserir(20);
-    b.inserir(50);
     b.inserir(10);
-    b.inserir(25);
-    b.inserir(40);
+    b.inserir(30);
+    b.inserir(5);
+    b.inserir(8);
+    b.inserir(7);
     b.inserir(26);
     b.inserir(27);
     cout << "\n";
     b.em_ordem_nivel_balanceamento(b.raiz);
 }
-// -1
-// -2
